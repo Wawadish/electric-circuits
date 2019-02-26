bfe1d6efb72d2b8ee1ebd05f301b7c7aa18c917d
cstuser <cstuser>
2019-02-26 10:42:02 -0500

    Drag and Drop feature implemented

--- src/electric/circuits/Main.java
+++ src/electric/circuits/Main.java
@@ -6,11 +6,13 @@
 package electric.circuits;
 
 import javafx.application.Application;
-import javafx.geometry.Rectangle2D;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 /**
@@ -21,6 +23,8 @@ public class Main extends Application {
 
     private HBox hbox = new HBox();
     private VBox vbox = new VBox();
+    public static StackPane stackPane = new StackPane();
+    public static Pane pane = new Pane();
     public static final int WIDTH = 1280, HEIGHT = 960;
 
     public static void main(String args[]) {
@@ -31,11 +35,25 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         vbox.getChildren().addAll(new MenuPane(), new SandboxPane(), new InfoPane());
         hbox.getChildren().addAll(new SideBarPane(), vbox);
+        stackPane.getChildren().add(hbox);
+        stackPane.getChildren().add(pane);
 
+        pane.setPickOnBounds(false);
+
+        stackPane.setPrefHeight(vbox.getHeight());
+        stackPane.setPrefWidth(hbox.getWidth());
 
         primaryStage.setWidth(WIDTH);
         primaryStage.setHeight(HEIGHT);
-        Scene scene = new Scene(hbox, WIDTH, HEIGHT);
+
+        Scene scene = new Scene(stackPane, WIDTH, HEIGHT);
+
+        scene.addEventFilter(MouseEvent.MOUSE_RELEASED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                pane.getChildren().remove(SideBarPane.temporaryImage);
+            }
+        });
         primaryStage.setScene(scene);
         primaryStage.show();
         MenuPane pane = new MenuPane();
