238559f67a321913f4ad47cb6887246cbf8020ec
Totom3 <suptom3@gmail.com>
2019-03-07 09:24:26 -0500

    Cleanup main class

--- src/electric/circuits/Main.java
+++ src/electric/circuits/Main.java
@@ -1,15 +1,8 @@
 package electric.circuits;
 
-import electric.circuits.component.DummyComponent;
-import java.awt.Point;
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.geometry.Bounds;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -19,62 +12,30 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
-    private HBox hbox = new HBox();
-    private VBox vbox = new VBox();
-    public static StackPane stackPane = new StackPane();
-    public static Pane pane = new Pane();
-    public static final int WIDTH = 1280, HEIGHT = 960;
-    public static double draggedX;
-    public static double draggedY;
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 960;
 
-    private final SandboxPane sandboxPane = new SandboxPane();
+	private final MenuPane menuPane = new MenuPane();
+	private final InfoPane infoPane = new InfoPane();
+	private final SandboxPane sandboxPane = new SandboxPane();
+	private final SideBarPane sideBarPane = new SideBarPane();
 
-    public static void main(String args[]) {
-        launch(args);
-    }
+	public static void main(String args[]) {
+		launch(args);
+	}
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        vbox.getChildren().addAll(new MenuPane(), sandboxPane, new InfoPane());
-        hbox.getChildren().addAll(new SideBarPane(), vbox);
-        stackPane.getChildren().add(hbox);
-        stackPane.getChildren().add(pane);
+	@Override
+	public void start(Stage stage) throws Exception {
+		HBox hbox = new HBox();
+		VBox vbox = new VBox();
 
-        pane.setPickOnBounds(false);
+		vbox.getChildren().addAll(menuPane, sandboxPane, infoPane);
+		hbox.getChildren().addAll(sideBarPane, vbox);
 
-        stackPane.setPrefHeight(vbox.getHeight());
-        stackPane.setPrefWidth(hbox.getWidth());
-
-        primaryStage.setWidth(WIDTH);
-        primaryStage.setHeight(HEIGHT);
-
-        Scene scene = new Scene(stackPane, WIDTH, HEIGHT);
-
-//        scene.addEventFilter(MouseEvent.MOUSE_RELEASED, new EventHandler<MouseEvent>() {
-//            @Override
-//            public void handle(MouseEvent mouseEvent) {
-//                Bounds sandboxBoundsInScene = sandboxPane.localToScene(sandboxPane.getBoundsInLocal());
-//                System.out.println(sandboxBoundsInScene.getMinY() + " " + sandboxBoundsInScene.getMaxY());
-//                
-//                pane.getChildren().remove(SideBarPane.temporaryImage);
-//                draggedX = SideBarPane.temporaryImage.getX();
-//                draggedY = SideBarPane.temporaryImage.getY();
-//                
-//                if(draggedY > sandboxBoundsInScene.getMinY() && 
-//                        draggedY < sandboxBoundsInScene.getMaxY() &&
-//                        draggedX > sandboxBoundsInScene.getMinX()){
-//                    System.out.println("in bounds");
-//                    int gridX, gridY;
-//                    gridX = (int)((draggedX - sandboxBoundsInScene.getMinX())/30.0);
-//                    gridY = (int)((draggedY - sandboxBoundsInScene.getMinY())/30.0);
-//                    System.out.println("gridX : " + gridX + "\tgridY : " + gridY);
-//                    
-//                }
-//                
-//            }
-//        });
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        MenuPane pane = new MenuPane();
-    }
+		stage.setWidth(WIDTH);
+		stage.setHeight(HEIGHT);
+		
+		stage.setScene(new Scene(hbox, WIDTH, HEIGHT));
+		stage.show();
+	}
 }
