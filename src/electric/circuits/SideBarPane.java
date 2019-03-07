398c9f50849a2a7579516d64a4ca15454bd26761
Totom3 <suptom3@gmail.com>
2019-03-07 09:26:34 -0500

    Cleanup sidebarpane

--- src/electric/circuits/SideBarPane.java
+++ src/electric/circuits/SideBarPane.java
@@ -3,7 +3,6 @@ package electric.circuits;
 import electric.circuits.data.ComponentType;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.geometry.Pos;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
@@ -13,7 +12,6 @@ import javafx.scene.input.DataFormat;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.util.Callback;
 
 /**
@@ -22,64 +20,65 @@ import javafx.util.Callback;
  */
 public class SideBarPane extends Pane {
 
-    private static final Image PLACEHOLDER = new Image("file:assets/images/placeholder.png");
+	private static final Image PLACEHOLDER = new Image("file:assets/images/placeholder.png");
 
-    private final ListView<ListViewItem> listView = new ListView();
-    private final ObservableList<ListViewItem> listItems = FXCollections.observableArrayList();
+	private final ListView<ListViewItem> listView = new ListView();
 
-    public SideBarPane() {
-        //Setting the background color and dimensions of the sidebar
-        setStyle("-fx-background-color: grey;");
-        setPrefSize(Main.WIDTH / 5, Main.HEIGHT);
+	public SideBarPane() {
+		// Setting the background color and dimensions of the sidebar
+		setStyle("-fx-background-color: grey;");
+		setPrefSize(Main.WIDTH / 5, Main.HEIGHT);
 
-        // Add temporary components to the side list
-        listItems.add(new ListViewItem("Battery", PLACEHOLDER, ComponentType.BATTERY));
-        listItems.add(new ListViewItem("LED", PLACEHOLDER, ComponentType.LED));
-        listItems.add(new ListViewItem("Resistance", PLACEHOLDER, ComponentType.RESISTANCE));
+		ObservableList<ListViewItem> listItems = FXCollections.observableArrayList();
 
-        // Each cell of the ListView will have an image and text.
-        listView.setCellFactory(cellFactory());
+		// Add temporary components to the side list
+		listItems.add(new ListViewItem("Battery", PLACEHOLDER, ComponentType.BATTERY));
+		listItems.add(new ListViewItem("LED", PLACEHOLDER, ComponentType.LED));
+		listItems.add(new ListViewItem("Resistance", PLACEHOLDER, ComponentType.RESISTANCE));
 
-        // Executes whenever an element of the ListView is dragged.
-        listView.setOnDragDetected(e -> {
-            // Gets the currently selected item
-            ListViewItem item = listView.getSelectionModel().getSelectedItem();
-            
-            Dragboard db = listView.startDragAndDrop(TransferMode.COPY);
-            db.setDragView(item.getImage());
-            
-            ClipboardContent cc = new ClipboardContent();
-            cc.put(DataFormat.PLAIN_TEXT, item.getComponentType());
-            db.setContent(cc);
-        });
-        
-        //Adding ObservableItems to the ListView and defining the background color and dimensions of the ListView.
-        listView.setItems(listItems);
-        listView.setStyle("-fx-control-inner-background: grey;");
-        listView.setPrefSize(Main.WIDTH, Main.HEIGHT);
+		// Each cell of the ListView will have an image and text.
+		listView.setCellFactory(cellFactory());
 
-        //Adding the ListView to this Pane (the sidebar).
-        getChildren().add(listView);
-    }
+		// Executes whenever an element of the ListView is dragged.
+		listView.setOnDragDetected(e -> {
+			// Gets the currently selected item
+			ListViewItem item = listView.getSelectionModel().getSelectedItem();
 
-    private static Callback<ListView<ListViewItem>, ListCell<ListViewItem>> cellFactory() {
-        return param -> new ListCell<ListViewItem>() {
-            private ImageView imageView = new ImageView();
+			Dragboard db = listView.startDragAndDrop(TransferMode.COPY);
+			db.setDragView(item.getImage());
 
-            @Override
-            public void updateItem(ListViewItem lvi, boolean empty) {
-                super.updateItem(lvi, empty);
-                if (lvi == null || empty) {
-                    setText(null);
-                    setGraphic(null);
-                    return;
-                }
+			ClipboardContent cc = new ClipboardContent();
+			cc.put(DataFormat.PLAIN_TEXT, item.getComponentType());
+			db.setContent(cc);
+		});
 
-                setText(lvi.getName());
-                imageView.setImage(lvi.getImage());
-                setGraphic(imageView);
-            }
-        };
-    }
+		//Adding ObservableItems to the ListView and defining the background color and dimensions of the ListView.
+		listView.setItems(listItems);
+		listView.setStyle("-fx-control-inner-background: grey;");
+		listView.setPrefSize(Main.WIDTH, Main.HEIGHT);
+
+		//Adding the ListView to this Pane (the sidebar).
+		getChildren().add(listView);
+	}
+
+	private static Callback<ListView<ListViewItem>, ListCell<ListViewItem>> cellFactory() {
+		return param -> new ListCell<ListViewItem>() {
+			final ImageView imageView = new ImageView();
+
+			@Override
+			public void updateItem(ListViewItem lvi, boolean empty) {
+				super.updateItem(lvi, empty);
+				if (lvi == null || empty) {
+					setText(null);
+					setGraphic(null);
+					return;
+				}
+
+				setText(lvi.getName());
+				imageView.setImage(lvi.getImage());
+				setGraphic(imageView);
+			}
+		};
+	}
 
 }
